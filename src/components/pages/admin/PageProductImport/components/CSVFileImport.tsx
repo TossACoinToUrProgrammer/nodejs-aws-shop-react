13ef4b57d4d9e7629d786efd751f5a8a7d2692ef
Arlen Manasov <arlen.manasov@icloud.com>
2023-12-18 01:58:07 +0600

    completed task 7

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import axios from "axios";
+import { AlertContext } from "~/contexts/AlertContext/AlertContext";
 
 type CSVFileImportProps = {
   url: string;
@@ -9,6 +11,7 @@ type CSVFileImportProps = {
 
 export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const [file, setFile] = React.useState<File>();
+  const { showAlert } = useContext(AlertContext)
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
@@ -21,26 +24,42 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
   const removeFile = () => {
     setFile(undefined);
   };
-
   const uploadFile = async () => {
     console.log("uploadFile to", url);
+    try {
+      if (!file) return
 
-    // Get the presigned URL
-    // const response = await axios({
-    //   method: "GET",
-    //   url,
-    //   params: {
-    //     name: encodeURIComponent(file.name),
-    //   },
-    // });
-    // console.log("File to upload: ", file.name);
-    // console.log("Uploading to: ", response.data);
-    // const result = await fetch(response.data, {
-    //   method: "PUT",
-    //   body: file,
-    // });
-    // console.log("Result: ", result);
-    // setFile("");
+      const authorization_token = localStorage.getItem('authorization_token')
+      if (!authorization_token) {
+        console.log("there is no 'authorization_token' in localStorage")
+        return
+      }
+
+      // Get the presigned URL
+      const response = await axios({
+        method: "GET",
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
+        headers: {
+          "Authorization": `Basic ${authorization_token}`
+        }
+      });
+      console.log("File to upload: ", file.name);
+      console.log("Uploading to: ", response.data);
+      const result = await fetch(response.data, {
+        method: "PUT",
+        body: file,
+      });
+      console.log("Result: ", result);
+      setFile(undefined);
+    } catch (error: any) {
+      if (error?.response && (error?.response?.status === 401 || error?.response?.status === 403)) {
+        showAlert(`File was not uploaded. Error message ${error.response.data?.message}`)
+      }
+    }
+
   };
   return (
     <Box>
